951e5d34967921ba2965ef82e5f6b506e198bba9
Lissandro kenziano <lissandromr@hotmail.com>
2023-07-09 11:10:55 -0300

    init : estrutura inicial projeto react native

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screen/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world com react native</Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screen/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+const Home = () =>{
+    return(
+        <View>
+            <Text>
+                Meu primeiro component native
+            </Text>
+        </View>
+    )
+}
+export default Home
